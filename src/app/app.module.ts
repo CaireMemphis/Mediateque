f825b602d8a46ffaed3c0e47af37381367a75b69
Thomas Screve <thomas-scrve@laposte.net>
2022-06-23 13:27:47 +0200

    arborescence preparé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/commons/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/commons/footer/footer.component';
+import { NotFoundComponent } from './components/commons/not-found/not-found.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { ProductsPageComponent } from './components/pages/products-page/products-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HomePageComponent,
+    ProductsPageComponent
   ],
   imports: [
-    BrowserModule
+  
+    BrowserModule,
+  
+    FormsModule,
+    
+     ReactiveFormsModule,
+   
+     BrowserAnimationsModule,
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
