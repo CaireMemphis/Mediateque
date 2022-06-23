29644f9e7b36565cc47933af6f7b0f93f12b07ab
Thomas Screve <thomas-scrve@laposte.net>
2022-06-23 16:13:29 +0200

    creation list et card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { FooterComponent } from './components/commons/footer/footer.component';
 import { NotFoundComponent } from './components/commons/not-found/not-found.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
 import { ProductsPageComponent } from './components/pages/products-page/products-page.component';
+import { ProductListComponent } from './components/pages/products-page/product-list/product-list.component';
+import { ProductCardComponent } from './components/pages/products-page/product-list/product-card/product-card.component';
 
 
 @NgModule({
@@ -20,7 +22,9 @@ import { ProductsPageComponent } from './components/pages/products-page/products
     FooterComponent,
     NotFoundComponent,
     HomePageComponent,
-    ProductsPageComponent
+    ProductsPageComponent,
+    ProductListComponent,
+    ProductCardComponent
   ],
   imports: [
   
